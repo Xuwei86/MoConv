a411a975ef2639ab70e3259b778e3e2038b6916d
Xuwei86 <101174420+Xuwei86@users.noreply.github.com>
2024-08-19 11:13:08 +0800

    Add files via upload

---
+++ base_module.py
@@ -0,0 +1,88 @@
+import torch.nn as nn
+import torch
+from functools import partial
+import  math
+
+def get_act(act_layer='relu'):
+	act_dict = {
+		'none': nn.Identity,
+		'sigmoid': h_sigmoid,
+		'relu': nn.ReLU,
+		'silu': nn.SiLU
+	}
+	return act_dict[act_layer]
+
+class h_sigmoid(nn.Module):
+    def __init__(self,inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+    def forward(self, x):
+        return self.relu(x + 3)/6
+
+class ConvNormAct(nn.Module):
+
+    def __init__(self, dim_in, dim_out, kernel_size, stride=1, dilation=1, groups=1, bias=False,
+                 skip=False, norm_layer='bn_2d', act_layer='relu', inplace=True, drop_path_rate=0.):
+        super(ConvNormAct, self).__init__()
+        self.has_skip = skip and dim_in == dim_out
+        padding = math.ceil((kernel_size - stride) / 2)
+        self.conv = nn.Conv2d(dim_in, dim_out, kernel_size, stride, padding, dilation, groups, bias)
+        self.norm = get_norm(norm_layer)(dim_out)
+        self.act = get_act(act_layer)(inplace=inplace)
+        self.drop_path = DropPath(drop_path_rate) if drop_path_rate else nn.Identity()
+
+    def forward(self, x):
+        shortcut = x
+        x = self.conv(x)
+        x = self.norm(x)
+        x = self.act(x)
+        if self.has_skip:
+            x = self.drop_path(x) + shortcut
+        return x
+
+def get_norm(norm_layer='in_1d'):
+	eps = 1e-6
+	norm_dict = {
+		'none': nn.Identity,
+		# 'in_1d': partial(nn.InstanceNorm1d, eps=eps),
+		'in_2d': partial(nn.InstanceNorm2d, eps=eps),
+		# 'in_3d': partial(nn.InstanceNorm3d, eps=eps),
+		# 'bn_1d': partial(nn.BatchNorm1d, eps=eps),
+		'bn_2d': partial(nn.BatchNorm2d, eps=eps),
+		# 'bn_2d': partial(nn.SyncBatchNorm, eps=eps),
+		# 'bn_3d': partial(nn.BatchNorm3d, eps=eps),
+		'gn': partial(nn.GroupNorm, eps=eps),
+		'ln_1d': partial(nn.LayerNorm, eps=eps),
+		# 'ln_2d': partial(LayerNorm2d, eps=eps),
+        'ln_2d': partial(LayerNorm, eps=eps),
+	}
+	return norm_dict[norm_layer]
+
+
+class LayerNorm(nn.Module):
+    r""" LayerNorm that supports two data formats: channels_last (default) or channels_first.
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs
+    with shape (batch_size, channels, height, width).
+    """
+
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_first"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape), requires_grad=True)
+        self.bias = nn.Parameter(torch.zeros(normalized_shape), requires_grad=True)
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise ValueError(f"not support data format '{self.data_format}'")
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            # [batch_size, channels, height, width]
+            mean = x.mean(1, keepdim=True)
+            var = (x - mean).pow(2).mean(1, keepdim=True)
+            x = (x - mean) / torch.sqrt(var + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
